3e0d21c9bf5da4fb1bcdd258f2a42d568bc958e6
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-24 08:37:16 +0700

    updated value for new component

--- src/components/PostLists.jsx
+++ src/components/PostLists.jsx
@@ -1,12 +1,16 @@
 import Post from "./Posts";
+import NewPost from "./NewPost";
 import classes from "./PostLists.module.css"
 
 function PostList(){
     return (
-    <ul className={classes.posts}>
-        <Post author ='I am Baskoro'  body='React.js is awesome!!' />
-        <Post author ='I am Basran'  body='Its so exciting!!' />
-    </ul>
+    <>
+    <NewPost/>
+        <ul className={classes.posts}>
+            <Post author ='I am Baskoro'  body='React.js is awesome!!' />
+            <Post author ='I am Basran'  body='Its so exciting!!' />
+        </ul>
+    </>
 )}
 
 export default PostList;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,9 @@
+
 import classes from "./Posts.module.css"
 
 function Post(props){
     return (
         <div className ={classes.post}>
-            // eslint-disable-next-line react/prop-types
             <p className ={classes.author}>{props.author}</p>
             <p className ={classes.body}>{props.body}</p>
         </div>
