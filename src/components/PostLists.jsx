422c7fbdf7b77da46c7fc2e20f103438234d8b90
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-24 12:43:55 +0700

    add conditional state

--- src/components/PostLists.jsx
+++ src/components/PostLists.jsx
@@ -5,9 +5,14 @@ import Modal from './Modal';
 import classes from "./PostLists.module.css"
 
 function PostList(){
+    const [ modalIsVisible, setModalIsVisible ] = useState(true);
     const [ enteredBody, setEnteredBody ] = useState('');
     const [ enteredAuthor, setEnteredAuthor ] = useState('');
     
+    function hideModalHandler(){
+        setModalIsVisible(false);
+    } 
+
     function bodyChangeHandler(event){
         setEnteredBody(event.target.value);
     }
@@ -17,12 +22,14 @@ function PostList(){
     }
     return (
     <>
-    <Modal>
-        <NewPost 
-        onBodyChange={bodyChangeHandler} 
-        onAuthorChange={authorChangeHandler}
-        />
-    </Modal>
+    {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+            <NewPost 
+            onBodyChange={bodyChangeHandler} 
+            onAuthorChange={authorChangeHandler}
+            />
+        </Modal> 
+    ) : null}
         <ul className={classes.posts}>
             <Post author ={enteredAuthor}  body={enteredBody} />
             <Post author ='Mary'  body='I am exciting' />
