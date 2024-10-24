c76134f1246c02c1dd0fe6097e91987f7c8d99b5
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-24 09:16:03 +0700

    add prop to passing value between component

--- src/components/PostLists.jsx
+++ src/components/PostLists.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import Post from "./Posts";
 import NewPost from "./NewPost";
 import classes from "./PostLists.module.css"
 
 function PostList(){
+    const [ enteredBody, setEnteredBody ] = useState('');
+    const [ enteredAuthor, setEnteredAuthor ] = useState('');
+    
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event){
+        setEnteredAuthor(event.target.value);
+    }
     return (
     <>
-    <NewPost/>
+    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
         <ul className={classes.posts}>
-            <Post author ='I am Baskoro'  body='React.js is awesome!!' />
-            <Post author ='I am Basran'  body='Its so exciting!!' />
+            <Post author ={enteredAuthor}  body={enteredBody} />
+            <Post author ='Mary'  body='I am exciting' />
         </ul>
     </>
 )}
