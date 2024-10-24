a13ba60a7e4b6e694ae9b70204792c631e462de9
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-24 21:34:20 +0700

    adding routerLayout

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import NewPost from './components/NewPost.jsx'
+import RouterLayout from '../router/RootLayout.jsx'
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path : '/create-post', element: <NewPost/> },
+  { path:'/', element: <RouterLayout />, 
+    children: [
+      { path: '/', element: <App /> },
+      { path : '/create-post', element: <NewPost/> },
+    ], 
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
