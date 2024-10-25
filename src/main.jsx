71c251f0ab93db94eda862cb39c758a24990d8a1
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-25 17:14:12 +0700

    add router and change posts route

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import Posts from './router/Posts.jsx' 
+import Posts, { loader as postsLoader } from './router/Posts.jsx' 
 import './index.css'
 import NewPost from './router/NewPost.jsx'
 import RouterLayout from './router/RootLayout.jsx'
@@ -12,6 +12,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', 
         element: <Posts />, 
+        loader : postsLoader,
         children: [{ path : '/create-post', element: <NewPost/> },
       ] },
     ], 
