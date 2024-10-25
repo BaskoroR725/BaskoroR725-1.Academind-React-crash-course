d3959024159f99f8f901384be5df8e5741c68dab
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-25 17:59:20 +0700

    refactor newpost and add react route feature

--- src/components/PostLists.jsx
+++ src/components/PostLists.jsx
@@ -3,35 +3,26 @@ import { useLoaderData } from "react-router-dom";
 import Post from './Post'
 import classes from "./PostLists.module.css"
 
-function PostList(){
+function PostsList() {
     const posts = useLoaderData();
-    
-    function addPostHandler(postData){
-        fetch('http://localhost:8080/posts', {
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        setPosts((existingPost) => [postData, ...existingPost])
-    }
-
+  
     return (
-    <>
-    {posts.length > 0 && (
-        <ul className={classes.posts}>
-            {posts.map((post) => <Post key={post.body} author = {post.author} body={post.body} />)}
-        </ul>
-    )}
-
-    { posts.length === 0 && (
-        <div style={{textAlign:'center', color:'white'}}>
-            <h2>There are no post yet.</h2>
-            <p>Start adding some post </p>
-        </div>
-    )}
-    </>
-)}
-
-export default PostList;
+      <>
+        {posts.length > 0 && (
+          <ul className={classes.posts}>
+            {posts.map((post) => (
+              <Post key={post.body} author={post.author} body={post.body} />
+            ))}
+          </ul>
+        )}
+        {posts.length === 0 && (
+          <div style={{ textAlign: 'center', color: 'white' }}>
+            <h2>There are no posts yet.</h2>
+            <p>Start adding some!</p>
+          </div>
+        )}
+      </>
+    );
+  }
+  
+  export default PostsList;
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Posts, { loader as postsLoader } from './router/Posts.jsx' 
-import './index.css'
-import NewPost from './router/NewPost.jsx'
+import NewPost, { action as newPostAction } from './router/NewPost.jsx'
 import RouterLayout from './router/RootLayout.jsx'
+import './index.css'
 
 const router = createBrowserRouter([
-  { path:'/', element: <RouterLayout />, 
+  { path:'/', 
+    element: <RouterLayout />, 
     children: [
       { path: '/', 
         element: <Posts />, 
         loader : postsLoader,
-        children: [{ path : '/create-post', element: <NewPost/> },
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
       ] },
     ], 
   },
